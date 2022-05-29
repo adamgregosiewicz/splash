e87d389158fdfdbb27727f81ec89cb235b09a2f4
Adam Gregosiewicz <adam.gregosiewicz@gmail.com>
2022-05-29 23:41:48 +0200

    Extract function for droping outliers

--- splash_data.py
+++ splash_data.py
@@ -19,6 +19,9 @@ def outliers_of_df(df):
 
     return outliers
 
+def drop_outliers(df, column):
+    return df.drop(outliers_of_df(df[column]).index.tolist())
+
 
 def mean_std_sp_to_hsc_cardinality(sticky_paper_df, high_speed_camera_df, sample_count):
     """
@@ -36,14 +39,14 @@ def mean_std_sp_to_hsc_cardinality(sticky_paper_df, high_speed_camera_df, sample
     return np.mean(sp_hsc_quotient), np.std(sp_hsc_quotient)
 
 
-high_speed_cam_df = pd.read_csv("high_speed_camera_1529.csv")
+high_speed_camera_df = pd.read_csv("high_speed_camera_1529.csv")
 sticky_paper_df = pd.read_csv("sp_1529.csv")
 
 #high_speed_cam_df = pd.read_csv("hsc_15.csv")
 #sticky_paper_df = pd.read_csv("sp_15.csv")
 
 # count splashes from 0
-high_speed_cam_df['no'] -= 1
+high_speed_camera_df['no'] -= 1
 sticky_paper_df['no'] -= 1
 
 # indicies of sticky paper registered by hsc
@@ -51,19 +54,19 @@ no_of_hsc = [1,2,3,4,5,7,8,9,10,11,12,13,14,16,17,18]
 
 
 # outliers handling
-high_speed_cam_df = high_speed_cam_df.drop(outliers_of_df(high_speed_cam_df['v']).index.tolist())
+high_speed_camera_df = drop_outliers(high_speed_camera_df, 'v')
 
 
 
-energy_sum_df = pd.DataFrame([high_speed_cam_df[high_speed_cam_df['no'] == i]['e'].sum() for i in range(16)], columns = ['energy_sum'])
+energy_sum_df = pd.DataFrame([high_speed_camera_df[high_speed_camera_df['no'] == i]['e'].sum() for i in range(16)], columns = ['energy_sum'])
 
 energy_mean = energy_sum_df.mean()[0]
 energy_std = energy_sum_df.std()[0]
 energy_min = energy_sum_df.min()[0]
 energy_max = energy_sum_df.max()[0]
 
-energy_p_min = high_speed_cam_df['e'].min()
-energy_p_max = high_speed_cam_df['e'].max()
+energy_p_min = high_speed_camera_df['e'].min()
+energy_p_max = high_speed_camera_df['e'].max()
 
 print(f"Energy mean = {energy_mean}, std = {energy_std}")
 print(f"Energy min = {energy_min}, max = {energy_max}\n")
@@ -71,7 +74,7 @@ print(f"Energy min = {energy_min}, max = {energy_max}\n")
 print(f"Particle energy min = {energy_p_min}, max = {energy_p_max}\n")
 
 
-scaling_mean, scaling_std = mean_std_sp_to_hsc_cardinality(sticky_paper_df, high_speed_cam_df, 1000)
+scaling_mean, scaling_std = mean_std_sp_to_hsc_cardinality(sticky_paper_df, high_speed_camera_df, 1000)
 
 energy_scaled_mean = energy_mean * scaling_mean
 energy_scaled_std = energy_std * scaling_mean
